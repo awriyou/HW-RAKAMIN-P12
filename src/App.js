7552d15682e2033de519cf492367566439636d3b
awriyou <febriaryo@gmail.com>
2023-11-02 23:21:15 +0700

    Mengupload final work tugas pada P12 membuat game tictactoe

--- src/App.js
+++ src/App.js
@@ -1,47 +1,78 @@
 
-import * as React from 'react';
+import React, { useState } from "react";
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
+  function selectSquare(square) {
+    if (squares[square] || calculateWinner(squares)) return;
+    const nextSquares = squares.slice();
+    nextSquares[square] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   function restart() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
   }
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
-      </button>
+      <>
+        <div
+          className=" bg-teal-400 w-8 h-6 my-2 text-white"
+          onClick={() => selectSquare(i)}
+        >
+          {squares[i]}
+        </div>
+      </>
     );
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else if (squares.every(Boolean)) {
+    status = "Scratch: Cat's game";
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
-    <div>
-      <div >STATUS</div>
-      <div >
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+    <div className="container bg-slate-200 text-center pt-10">
+      <div>{status}</div>
+      <div className="bg-white mt-4 shadow-xl rounded-xl w-1/2 text-center mx-auto py-4 flex gap-4 justify-center">
+        <div className="">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
       </div>
-      <div >
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div >
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-      <button onClick={restart}>
-        restart
+      <button
+        className="bg-teal-300 my-10 px-4 py-1 text-sm font-thin hover:bg-teal-600 hover:text-white rounded-lg"
+        onClick={restart}
+      >
+        Restart Game
       </button>
     </div>
   );
 }
+
+// Kode sisanya (Game, calculateWinner, dan sebagainya) sama seperti kode pertama
+
 
 function Game() {
   return (
@@ -53,19 +84,9 @@ function Game() {
   );
 }
 
-// eslint-disable-next-line no-unused-vars
-function calculateStatus(winner, squares, nextValue) {
-  return winner
-    ? `Winner: ${winner}`
-    : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
-}
 
-// eslint-disable-next-line no-unused-vars
-function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
-}
+
+
 
 // eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
